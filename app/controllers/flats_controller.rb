2a34f8e59913bd34db986db0b17252b1704f4188
kevin robert <kevin.j.robert@gmail.com>
2018-11-19 18:37:30 +0100

    static flats app

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,18 @@
+require "open-uri"
+class FlatsController < ApplicationController
+  before_action :set_flats
+
+  def index
+  end
+
+  def show
+    @flat = @flats.find { |flat| flat["id"] == params[:id].to_i }
+  end
+
+  private
+
+  def set_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
+  end
+end
